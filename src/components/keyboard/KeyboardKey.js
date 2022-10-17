2ffe8a4fdf257304f76eabfb1939caa4d5e426db
jordnkr <jordnkr408@gmail.com>
2022-10-17 01:30:30 -0500

    file formatting

--- src/components/keyboard/KeyboardKey.js
+++ src/components/keyboard/KeyboardKey.js
@@ -1,15 +1,22 @@
-import React from 'react';
+import React from "react";
 
-import classes from './KeyboardKey.module.css';
+import classes from "./KeyboardKey.module.css";
 
 const KeyboardKey = (props) => {
   const clickHandler = (e) => {
     const text = e.target.textContent;
     e.target.blur();
     props.onButtonPress(text);
-  }
+  };
 
-  return ( <button onClick={clickHandler} className={`${classes.button} ${classes[props.color]}`}>{props.value}</button> );
-}
- 
+  return (
+    <button
+      onClick={clickHandler}
+      className={`${classes.button} ${classes[props.color]}`}
+    >
+      {props.value}
+    </button>
+  );
+};
+
 export default KeyboardKey;
